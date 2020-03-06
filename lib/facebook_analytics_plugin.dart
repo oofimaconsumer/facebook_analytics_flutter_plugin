54e279385fc96d8662cf7b5535b8b5d33e66ce72
oofimaconsumer <avcdoooo@gmail.com>
2020-03-06 12:49:47 +0500

    add new methods

--- lib/facebook_analytics_plugin.dart
+++ lib/facebook_analytics_plugin.dart
@@ -2,26 +2,72 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-
-
 class FacebookAnalyticsPlugin {
   static const MethodChannel _channel =
       const MethodChannel('facebook_analytics_plugin');
 
-  /// The achievement of specific levels you define within your application, business or organization.
-  static Future<void> logAchievedLevel({@required String level})async{
-    assert(level.isNotEmpty);
-    await _channel.invokeMethod("EVENT_NAME_ACHIEVED_LEVEL", level);
-  }
+  // Public
 
-  /// Logs a custom Facebook event with the given [name] and event [parameters].
+  /// Logs a custom Facebook event with the given [name] and event [parameters]
   static Future<void> logCustomEvent(
       {@required String name, Map<String, dynamic> parameters}) async {
     if (parameters == null) {
       parameters = {};
     }
+    await _channel.invokeMethod(
+        'logCustomEvent', {"name": name, "parameters": parameters});
+  }
 
+  /// Logs ...
+  static Future<void> logCompletedRegistration() async {
+    await _channel.invokeMethod("logCompletedRegistration");
+  }
+
+  /// Logs ...
+  static Future<void> logCompletedPurchaseWith({@required double amount, @required String currency}) async {
     await _channel
-        .invokeMethod('logCustomEvent', {"name": name, "parameters": parameters});
+        .invokeMethod("logCompletedPurchase", {"amount": amount, "currency": currency});
+  }
+
+  /// ...
+  static Future<void> setAndHash({
+    String email,
+    String firstName,
+    String lastName,
+    String phone,
+    String dateOfBirth,
+    String gender,
+    String city,
+    String state,
+    String zip,
+    String country,
+  }) async {
+    await _channel.invokeMethod("setUserData", {
+      "parameters": _captureNonnullFrom(map: {
+        "email": email,
+        "firstName": firstName,
+        "lastName": lastName,
+        "phone": phone,
+        "dateOfBirth": dateOfBirth,
+        "gender": gender,
+        "city": city,
+        "state": state,
+        "zip": zip,
+        "country": country,
+      })
+    });
+  }
+
+  // Private
+
+  static Map<String, dynamic> _captureNonnullFrom(
+      {@required Map<String, dynamic> map}) {
+    Map<String, dynamic> captured = {};
+    map.forEach((key, value) {
+      if (value != null) {
+        captured[key] = value;
+      }
+    });
+    return captured;
   }
 }
